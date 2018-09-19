75e0fe58b3ad9c3d1d383dbf72cbb004fcec3acd
ramirocervera <recerveraportillo@hotmail.com>
2018-09-19 01:10:09 -0300

    Add files via upload

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,19 +1,22 @@
 export class Stock {
     constructor(
         public Symbol: string,
-        public Name: string,
         public bidPrice: number,
         public bidQuantity: number,
         public askPrice: number,
         public askQuantity: number,
-        public Price: number,
+        public lastPrice : number,
         public ChangeinPercentage: number,
         public PriceOpen: number,
         public MinimumPrice: number,
         public MaximumPrice: number,
         public PreviousClose: number,
-        public Volumen: number,
+        public NominalVolumen: number,
         public OperatedAmount: number,
         public OperationQuantity: number) {
+    }
+
+    getChangeinPercentage(PreviousClose : number, lastPrice : number) : number {
+        return  (PreviousClose - lastPrice) / lastPrice * 100;
     }
 }
--- src/app/stock-panel/stock-panel.component.ts
+++ src/app/stock-panel/stock-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../model';
 
 @Component({
   selector: 'app-stock-panel',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class StockPanelComponent implements OnInit {
-
-  constructor() { }
+  stock : Stock;
+  constructor() { 
+  }
 
   ngOnInit() {
+    this.stock = this.InitialStockValues(); 
+  }
+  InitialStockValues(): Stock {
+    let initialStock = new Stock('AGRO', 350, 13.700, 793, 14.250, 13.900
+    , null, 13.650, 14.250, 13.650, 13.650, 158839, 2230857, 191)
+
+    initialStock.getChangeinPercentage(initialStock.PreviousClose, initialStock.lastPrice)
+    
+    return initialStock; 
   }
 
 }
